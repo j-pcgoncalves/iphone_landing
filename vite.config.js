168ae592659cccc60957c3bcbb556dcf3dc33c82
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2024-07-10 18:44:38 +0100

    configure sentry

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "personal-9wu",
+    project: "iphone-landing"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 })
