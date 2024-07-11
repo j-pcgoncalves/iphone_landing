591b6be8afce3b472c20f73fbcedb6356c80ef73
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2024-07-11 23:14:48 +0100

    create and style VideoCarousel component

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -3,6 +3,7 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import { useEffect, useRef, useState } from "react";
 import { highlightsSlides } from "../constants";
+import { pausetImg, playImg, replayImg } from "../utils";
 gsap.registerPlugin(ScrollTrigger);
 
 const VideoCarousel = () => {
@@ -112,6 +113,112 @@ const VideoCarousel = () => {
             }
         }
     }, [videoId, startPlay]);
-}
+
+    useEffect(() => {
+        if (loadedData.length > 3) {
+            if (!isPlaying) {
+                videoRef.current[videoId].pause();
+            } else {
+                startPlay && videoRef.current[videoId].play();
+            }
+        }
+    }, [startPlay, videoId, isPlaying, loadedData]);
+
+    const handleProcess = (type, i) => {
+        switch (type) {
+            case "video-end":
+                setVideo((pre) => ({ ...pre, isEnd: true, videoId: i + 1 }));
+                break;
+            
+            case "video-last":
+                setVideo((pre) => ({ ...pre, isLastVideo: true }));
+                break;
+            
+            case "video-reset":
+                setVideo((pre) => ({ ...pre, videoId: 0, isLastVideo: false }));
+                break;
+            
+            case "pause":
+                setVideo((pre) => ({ ...pre, isPlaying: !pre.isPlaying }));
+                break;
+            
+            case "play":
+                setVideo((pre) => ({ ...pre, isPlaying: !pre.isPlaying }));
+                break;
+            
+            default:
+                return video;
+        }
+    };
+
+    const handleLoadedMetaData = (i, e) => setLoadedData((pre) => [...pre, e]);
+
+    return (
+        <>
+            <div className="flex items-center">
+                {highlightsSlides.map((list, i) => (
+                    <div key={list.id} id="slider" className="sm:pr-20 pr-10">
+                        <div className="video-carousel_container">
+                            <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+                                <video
+                                    id="video"
+                                    playsInline={true}
+                                    className={`${list.id === 2 && "translate-x-44"} pointer-events-none`}
+                                    preload="auto"
+                                    muted
+                                    ref={(el) => (videoRef.current[i] = el)}
+                                    onEnded={() => i !== 3 ? handleProcess("video-end", i) : handleProcess("video-last")}
+                                    onPlay={() => setVideo((pre) => ({ ...pre, isPlaying: true }))}
+                                    onLoadedMetadata={(e) => handleLoadedMetaData(i, e)}
+                                >
+                                    <source src={list.video} type="video/mp4" />
+                                </video>
+                            </div>
+
+                            <div className="absolute top-12 left-[5%] z-10">
+                                {list.textLists.map((text, i) => (
+                                    <p key={i} className="md:text-2xl text-xl font-medium">
+                                        {text}
+                                    </p>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <div className="relative flex-center mt-10">
+                <div className="flex-center py-5 px-7 bg-gray-300 backdrop-blur rounded-full">
+                    {videoRef.current.map((_, i) => (
+                        <span
+                            key={i}
+                            className="mx-2 w-3 h-3 bg-gray-200 rounded-full relative cursor-pointer"
+                            ref={(el) => (videoDivRef.current[i] = el)}
+                        >
+                            <span 
+                                className="absolute h-full w-full rounded-full"
+                                ref={(el) => (videoSpanRef.current[i] = el)}
+                            />
+                        </span>
+                    ))}
+                </div>
+
+                <button className="control-btn">
+                    <img 
+                        src={isLastVideo ? replayImg : !isPlaying ? playImg : pausetImg}
+                        alt={isLastVideo ? "Replay" : !isPlaying ? "Play" : "Pause"}
+                        onClick={
+                            isLastVideo
+                                ? () => handleProcess("video-reset")
+                                : !isPlaying
+                                ? () => handleProcess("play")
+                                : () => handleProcess("pause")
+                        }
+                    />
+                </button>
+            </div>
+        </>
+    );
+};
 
 export default VideoCarousel;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,8 @@
 import hmv from "/assets/videos/hero.mp4";
 import smallhmv from "/assets/videos/smallHero.mp4";
 import highlightFirstmv from "/assets/videos/highlight-first.mp4";
-import highlightSectmv from "/assets/videos/hightlight-sec.mp4";
-import highlightThirdmv from "/assets/videos/hightlight-third.mp4";
+import highlightSectmv from "/assets/videos/hightlight-third.mp4";
+import highlightThirdmv from "/assets/videos/hightlight-sec.mp4";
 import highlightFourthmv from "/assets/videos/hightlight-fourth.mp4";
 
 import apple from "/assets/images/apple.svg";
@@ -10,6 +10,9 @@ import search from "/assets/images/search.svg";
 import bag from "/assets/images/bag.svg";
 import watch from "/assets/images/watch.svg";
 import right from "/assets/images/right.svg";
+import replay from "/assets/images/replay.svg";
+import play from "/assets/images/play.svg";
+import pause from "/assets/images/pause.svg";
 
 export const heroVideo = hmv;
 export const smalHeroVideo = smallhmv;
@@ -23,3 +26,6 @@ export const searchImg = search;
 export const bagImg = bag;
 export const watchImg = watch;
 export const rightImg = right;
+export const replayImg = replay;
+export const playImg = play;
+export const pausetImg = pause;
