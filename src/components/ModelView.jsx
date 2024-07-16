487067b3ada682ba0e3c64dd5bad0fda099cdc6f
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2024-07-16 20:00:05 +0100

    create Loader and IPhone components, finish ModelView component

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,5 +1,10 @@
-import { PerspectiveCamera, View } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei";
+import * as THREE from "three";
+
 import Lights from "./Lights";
+import IPhone from "./IPhone";
+import { Suspense } from "react";
+import Loader from "./Loader";
 
 const ModelView = ({ 
     index, 
@@ -22,6 +27,26 @@ const ModelView = ({
             <PerspectiveCamera makeDefault position={[0, 0, 4]} />
 
             <Lights />
+
+            <OrbitControls 
+                makeDefault
+                ref={controlRef}
+                enableZoom={false}
+                enablePan={false}
+                rotateSpeed={0.4}
+                target={new THREE.Vector3(0, 0, 0)}
+                onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+            />
+
+            <group ref={groupRef} name={`${index === 1 ? "small" : "large"}`} position={[0, 0, 0]}>
+                <Suspense fallback={<Loader />}>
+                    <IPhone 
+                        scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+                        item={item}
+                        size={size}
+                    />
+                </Suspense>
+            </group>
         </View>
     );
 }
