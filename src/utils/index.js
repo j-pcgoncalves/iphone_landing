9906f201e75ef6e5c807a4926ab87ef4c0fe7fd7
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2024-07-18 19:52:28 +0100

    create, style and animate Features component

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,6 +4,7 @@ import highlightFirstmv from "/assets/videos/highlight-first.mp4";
 import highlightSectmv from "/assets/videos/hightlight-third.mp4";
 import highlightThirdmv from "/assets/videos/hightlight-sec.mp4";
 import highlightFourthmv from "/assets/videos/hightlight-fourth.mp4";
+import exploremv from "/assets/videos/explore.mp4";
 
 import apple from "/assets/images/apple.svg";
 import search from "/assets/images/search.svg";
@@ -18,6 +19,8 @@ import yellow from "/assets/images/yellow.jpg";
 import blue from "/assets/images/blue.jpg";
 import white from "/assets/images/white.jpg";
 import black from "/assets/images/black.jpg";
+import explore1 from "/assets/images/explore1.jpg";
+import explore2 from "/assets/images/explore2.jpg";
 
 export const heroVideo = hmv;
 export const smalHeroVideo = smallhmv;
@@ -25,6 +28,7 @@ export const highlightFirstVideo = highlightFirstmv;
 export const highlightSecondVideo = highlightSectmv;
 export const highlightThirdVideo = highlightThirdmv;
 export const highlightFourthVideo = highlightFourthmv;
+export const exploreVideo = exploremv;
 
 export const appleImg = apple;
 export const searchImg = search;
@@ -39,3 +43,5 @@ export const yellowImg = yellow;
 export const blueImg = blue;
 export const whiteImg = white;
 export const blackImg = black;
+export const explore1Img = explore1;
+export const explore2Img = explore2;
