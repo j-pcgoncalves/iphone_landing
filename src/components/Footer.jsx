7a5161bf2b4ce23798f3a7d8d63a020e1e6cb57a
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2024-07-18 21:37:31 +0100

    make footer links size more responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
                     <p className="font-semibold text-gray text-xs">Copyright @ 2024 Apple Inc. All rights reserved.</p>
                     <div className="flex">
                         {footerLinks.map((link, i) => (
-                            <p key={link} className="font-semibold text-gray text-xs">
+                            <p key={link} className="font-semibold text-gray text-[0.6rem] sm:text-xs">
                                 {link}{" "}
                                 {i !== footerLinks.length - 1 && (
                                     <span className="mx-2"> | </span>
